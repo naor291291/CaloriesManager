4191472ea92c4015da8968097f08984a8647e289
adimalka14 <122941157+adimalka14@users.noreply.github.com>
2024-06-18 16:39:32 +0300

    changing the name from  Adi--- to Adi

--- routes/about.js
+++ routes/about.js
@@ -5,7 +5,7 @@ router.get('/', async (req, res) => {
     try {
         const developers = [
             { firstname: 'Naor', lastname: 'Abudi', id: 203571039, email: 'naor2912@gmail.com' },
-            { firstname: 'Adi---', lastname: 'Malka', id: 315426684, email: 'adimalka14@gmail.com' }
+            { firstname: 'Adi', lastname: 'Malka', id: 315426684, email: 'adimalka14@gmail.com' }
         ];
         res.json(developers);
     } catch (err) {
